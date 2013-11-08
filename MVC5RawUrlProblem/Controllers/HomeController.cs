14635ef003377cec358dda44e225e6418179fe17
Will <doggy.huang@gmail.com>
2013-11-09 00:41:32 +0800

    首次將專案加入 Git 本地儲存庫

---
+++ MVC5RawUrlProblem/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using MVC5RawUrlProblem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5RawUrlProblem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult 登入()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult 登入(LoginViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+
+                //return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ MVC5RawUrlProblem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5RawUrlProblem.Startup))]
+namespace MVC5RawUrlProblem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
